da66b54c661feedd434c0f1bcd71a059bb89f6eb
Christopher L Miller <moose51789@gmail.com>
2019-05-20 18:45:02 -0400

    fix urlBuilder on case that nothing was provided

--- utils/url.tsx
+++ utils/url.tsx
@@ -8,29 +8,33 @@ function serialize(obj) {
 }
 
 export function urlBuilder(current, slug, value) {
-  // decode the url into the path and the params
-  const [path, paramString] = current.split(`?`);
+  // check that we have any params first
+  if (current.indexOf(`?`) != -1) {
+    // decode the url into the path and the params
+    const [path, paramString] = current.split(`?`);
 
-  // split the params out into array
-  const params = paramString.split(`&`);
+    // split the params out into array
+    const params = paramString.split(`&`);
 
-  // start with an empty array
-  const paramsObject = [];
+    // start with an empty array
+    const paramsObject = [];
 
-  // loop each of the params and split it out, push the key and value onto the array
-  params.forEach(param => {
-    const [key, value] = param.split(`=`);
-    paramsObject[key] = value;
-  });
+    // loop each of the params and split it out, push the key and value onto the array
+    params.forEach(param => {
+      const [key, value] = param.split(`=`);
+      paramsObject[key] = value;
+    });
 
-  // at this point we can append our key and value passed in onto the object
-  // but only if the value isn't empty
-  if (value != ``) {
-    paramsObject[slug] = value;
-  } else {
-    delete paramsObject[slug];
+    // at this point we can append our key and value passed in onto the object
+    // but only if the value isn't empty
+    if (value != ``) {
+      paramsObject[slug] = value;
+    } else {
+      delete paramsObject[slug];
+    }
+
+    // lastly serialize the object, append the current path to it, and return that!
+    return `${path}${serialize(paramsObject)}`;
   }
-
-  // lastly serialize the object, append the current path to it, and return that!
-  return `${path}${serialize(paramsObject)}`;
+  return `${current}?${slug}=${value}`;
 }
