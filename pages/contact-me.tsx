d82aca492cf0063661dab71d61aab4ba7e5b700f
ChristopherLMiller <chris@christopherleemiller.me>
2019-09-27 08:23:51 -0400

    Fix Contact Page to be mobile friendly

--- pages/contact-me.tsx
+++ pages/contact-me.tsx
@@ -7,6 +7,7 @@ import { withLayout } from '../components/layout/withLayout';
 import Card from '../components/Card';
 import { SocialLinks } from '../components/SocialLinks';
 import { Main } from '../styles/Generics';
+import { Props } from '../styles/Themes';
 
 const title = `Contact Me`;
 const description = `How to reach me with any comments, questions, and concerns regarding anything you see here!`;
@@ -20,15 +21,35 @@ const ContactSchema = Yup.object().shape({
   message: Yup.string().required(`What do you need help with?`),
 });
 
-const StyledGrid = styled.div`
+const ContactGrid = styled.div`
   display: grid;
-  grid-template-columns: 66% auto;
+  grid-template-columns: auto;
+
+  & > div:first-child {
+    border-bottom: 1px solid var(--background-dark);
+  }
+
+  @media (min-width: ${(props: Props) => props.theme.sizes.med_large}) {
+    & > div:first-child {
+      border-right: 1px solid var(--background-dark);
+    }
+    & > div:last-child {
+      padding-left: 5%;
+    }
+  }
+
+  @media (min-width: ${(props: Props) => props.theme.sizes.med_large}) {
+    grid-template-columns: auto 1fr;
+  }
+`;
+
+const ContactColumn = styled.div`
+  padding: 3% 5%;
 `;
 
 const StyledContactForm = styled.div`
   display: grid;
-  grid-template-columns: 50% 50%;
-
+  grid-template-columns: repeat(50%, 2);
   input,
   textarea {
     width: 100%;
@@ -42,6 +63,7 @@ const StyledContactForm = styled.div`
 
 const StyledContactFormFieldset = styled.fieldset`
   border: none;
+  padding-left: 0;
 `;
 
 const StyledContactFormLabel = styled.label`
@@ -53,6 +75,7 @@ const StyledContactFormFullWidth = styled.fieldset`
   grid-column-start: 1;
   grid-column-end: 3;
   border: none;
+  padding-left: 0;
 `;
 
 const StyledContactFormErrorMessage = styled.div`
@@ -86,79 +109,83 @@ const StyledButton = styled(PosedButton)`
 
 const ContactPage = () => (
   <Main>
-    <StyledGrid>
-      <Card
-        heading="Send me a message"
-        subHeading="Got a question?  Looking for some work to be done?  I'd love to hear from you.  Send me a message and I'll reply as soon as possible."
-      >
-        <Formik
-          initialValues={{ email: ``, name: ``, message: `` }}
-          onSubmit={values => {
-            fetch(`/api/contact`, {
-              method: `post`,
-              headers: {
-                Accept: `application/json`,
-                'Content-Type': `application/json`,
-              },
-              body: JSON.stringify(values),
-            }).then(res => {
-              console.log(res);
-              alert(`Thank you`);
-            });
-          }}
-          validationSchema={ContactSchema}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <StyledContactForm>
-                <StyledContactFormFieldset>
-                  <StyledContactFormLabel htmlFor="name">
-                    Name:{` `}
-                  </StyledContactFormLabel>
-                  <Field type="text" name="name" />
-                  <StyledContactFormErrorMessage>
-                    <ErrorMessage name="name" component="span" />
-                  </StyledContactFormErrorMessage>
-                </StyledContactFormFieldset>
-                <StyledContactFormFieldset>
-                  <StyledContactFormLabel htmlFor="email">
-                    Email:{` `}
-                  </StyledContactFormLabel>
-                  <Field type="email" name="email" />
-                  <StyledContactFormErrorMessage>
-                    <ErrorMessage name="email" component="span" />
-                  </StyledContactFormErrorMessage>
-                </StyledContactFormFieldset>
+    <Card
+      heading="Send me a message"
+      subHeading="Got a question?  Looking for some work to be done?  I'd love to hear from you.  Send me a message and I'll reply as soon as possible."
+      padding={false}
+    >
+      <ContactGrid>
+        <ContactColumn>
+          <Formik
+            initialValues={{ email: ``, name: ``, message: `` }}
+            onSubmit={values => {
+              fetch(`/api/contact`, {
+                method: `post`,
+                headers: {
+                  Accept: `application/json`,
+                  'Content-Type': `application/json`,
+                },
+                body: JSON.stringify(values),
+              }).then(res => {
+                console.log(res);
+                alert(`Thank you`);
+              });
+            }}
+            validationSchema={ContactSchema}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <StyledContactForm>
+                  <StyledContactFormFieldset>
+                    <StyledContactFormLabel htmlFor="name">
+                      Name:{` `}
+                    </StyledContactFormLabel>
+                    <Field type="text" name="name" />
+                    <StyledContactFormErrorMessage>
+                      <ErrorMessage name="name" component="span" />
+                    </StyledContactFormErrorMessage>
+                  </StyledContactFormFieldset>
+                  <StyledContactFormFieldset>
+                    <StyledContactFormLabel htmlFor="email">
+                      Email:{` `}
+                    </StyledContactFormLabel>
+                    <Field type="email" name="email" />
+                    <StyledContactFormErrorMessage>
+                      <ErrorMessage name="email" component="span" />
+                    </StyledContactFormErrorMessage>
+                  </StyledContactFormFieldset>
 
-                <StyledContactFormFullWidth>
-                  <StyledContactFormLabel htmlFor="message">
-                    Message:
-                  </StyledContactFormLabel>
-                  <Field component="textarea" name="message" rows="5" />
-                  <StyledContactFormErrorMessage>
-                    <ErrorMessage name="message" component="span" />
-                  </StyledContactFormErrorMessage>
-                </StyledContactFormFullWidth>
-              </StyledContactForm>
-              <StyledButton type="submit" aria-disabled={isSubmitting}>
-                Send It!
-              </StyledButton>
-            </Form>
-          )}
-        </Formik>
-      </Card>
-      <Card heading="Other Methods">
-        <SocialLinks />
-        <h3>Email</h3>
-        <p>
-          <a href="mailto:info@christopherleemiller.me">
-            info@christopherleemiller.me
-          </a>
-        </p>
-        <h3>Phone</h3>
-        <p>+1 (574) 370-2148</p>
-      </Card>
-    </StyledGrid>
+                  <StyledContactFormFullWidth>
+                    <StyledContactFormLabel htmlFor="message">
+                      Message:
+                    </StyledContactFormLabel>
+                    <Field component="textarea" name="message" rows="5" />
+                    <StyledContactFormErrorMessage>
+                      <ErrorMessage name="message" component="span" />
+                    </StyledContactFormErrorMessage>
+                  </StyledContactFormFullWidth>
+                </StyledContactForm>
+                <StyledButton type="submit" aria-disabled={isSubmitting}>
+                  Send It!
+                </StyledButton>
+              </Form>
+            )}
+          </Formik>
+        </ContactColumn>
+        <ContactColumn>
+          <h4>Or These Other Methods</h4>
+          <h3>Email</h3>
+          <p>
+            <a href="mailto:info@christopherleemiller.me">
+              info@christopherleemiller.me
+            </a>
+          </p>
+          <h3>Phone</h3>
+          <p>+1 (574) 370-2148</p>
+          <SocialLinks />
+        </ContactColumn>
+      </ContactGrid>
+    </Card>
   </Main>
 );
 
