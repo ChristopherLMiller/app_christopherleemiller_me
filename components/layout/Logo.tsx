f1dc21f768d1ddb666bda6199872b8683c254234
ChristopherLMiller <chris@christopherleemiller.me>
2019-11-20 03:20:12 -0500

    Reduce logo size for better nav fit

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -18,7 +18,7 @@ const StyledLogo = styled(LogoFilter)`
   display: none;
 
   @media (min-width: ${(props: Props) => props.theme.sizes.small}) {
-    width: 55%;
+    width: 45%;
     display: block;
   }
 `;
