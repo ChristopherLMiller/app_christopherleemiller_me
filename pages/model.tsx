fc90a6271250987b63479a3cb1566dfafc414f08
ChristopherLMiller <chris@christopherleemiller.me>
2019-11-22 08:52:00 -0500

    Revert changes to Query

--- pages/model.tsx
+++ pages/model.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React, { SFC } from 'react';
-import { Query } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 import { useRouter } from 'next/router';
 import { withLayout } from '../components/layout/withLayout';
 import { MODELS_QUERY } from '../utils/query';
@@ -16,74 +16,64 @@ const description = `Whether it plane, car or tank, its all here!`;
 const ModelPage: SFC = () => {
   getAuth();
   const router = useRouter();
+  const { loading, error, data } = useQuery<iData>(MODELS_QUERY, {
+    variables: {
+      where: {
+        slug_contains: router.query.slug,
+      },
+    },
 
-  console.log(router);
+  });
+
+  if (loading)
+    return (
+      <Main>
+        <p>Loading...</p>
+      </Main>
+    );
+  if (error) {
+    console.error(`Fetch Error: ${error.message}`);
+
+    return (
+      <Main>
+        <Card heading="Unable to load data">
+          <h2>{error.message}</h2>
+          <p>
+            Sorry. Something happened and we can't seem to load data right now.
+            Possibly you're offline and if not please let us know.
+          </p>
+        </Card>
+      </Main>
+    );
+  }
+
+  if (data !== undefined) {
+    if (data.models.length >= 1) {
+      return (
+        <Main>
+          {data.models.map(model => (
+            <Model model={model} key={model.id} />
+          ))}
+        </Main>
+      );
+    }
+  }
 
   return (
     <Main>
-      <Query<iData>
-        query={MODELS_QUERY}
-        variables={{
-          where: {
-            slug_contains: router.query.slug,
-          },
-        }}>
-        {({ loading, data, error }: any) => {
-          if (loading) {
-            return (
-              <p>Loading...</p>
-            )
-          }
-
-          // log an error and show something to the user
-          if (error) {
-            console.error(`Fetch Error: ${error.message}`);
-
-            return (
-              <Main>
-                <Card heading="Unable to load data">
-                  <h2>{error.message}</h2>
-                  <p>
-                    Sorry. Something happened and we can't seem to load data right now.
-                    Possibly you're offline and if not please let us know.
-                  </p>
-                </Card>
-              </Main>
-            );
-          }
-
-          // data is good!
-          if (data !== undefined) {
-            if (data.models.length >= 1) {
-              return (
-                <Main>
-                  {data.models.map((model: any) => (
-                    <Model model={model} key={model.id} />
-                  ))}
-                </Main>
-              );
-            }
-          }
-
-          return (
-            <Main>
-              <Card heading="404 Model Not Found.">
-                <p>
-                  Sorry. We seem to have lost this model somewhere in the interwebs. I
-                  don't know what happened to it! Some gremlin somewhere must have
-                  snatched it on me, that or I just never built this model to begin
-                  with. Hmmmmm.
-                </p>
-                <hr />
-                <Link href="/models">
-                  <a>View All Models</a>
-                </Link>
-              </Card>
-            </Main>
-          );
-        }}
-      </Query>
-    </Main >
-  )
+      <Card heading="404 Model Not Found.">
+        <p>
+          Sorry. We seem to have lost this model somewhere in the interwebs. I
+          don't know what happened to it! Some gremlin somewhere must have
+          snatched it on me, that or I just never built this model to begin
+          with. Hmmmmm.
+        </p>
+        <hr />
+        <Link href="/models">
+          <a>View All Models</a>
+        </Link>
+      </Card>
+    </Main>
+  );
 };
 export default withLayout(ModelPage, { title, description, useSEO: false });
