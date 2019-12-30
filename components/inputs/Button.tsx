98e77598b845642633a7e03f3e822634bf399804
ChristopherLMiller <chris@christopherleemiller.me>
2019-12-30 09:37:34 -0500

    fix dependencies

--- components/inputs/Button.tsx
+++ components/inputs/Button.tsx
@@ -11,15 +11,15 @@ const PosedButton = posed.button({
   },
 });
 
-export const Button = styled(PosedButton)`
-  background: ${(props: any) => props.disabled ? 'var(--background-dark)' : 'var(--main-color)'};
+export const Button = styled(PosedButton) <{ disabled: boolean }>`
+  background: ${props => props.disabled ? 'var(--background-dark)' : 'var(--main-color)'};
   padding: 10px 30px;
   transition: all 0.25s;
   text-align: center;
   margin: 20px 5px;
   max-width: 200px;
   letter-spacing: -1px;
-  cursor: ${(props: any) => props.disabled ? 'progress' : 'pointer'};
+  cursor: ${props => props.disabled ? 'progress' : 'pointer'};
   color: var(--text-color-light);
   font-size: 2rem;
   border: none;
