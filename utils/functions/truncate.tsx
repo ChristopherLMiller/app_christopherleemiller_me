86143ae6c21740c786cf815ff05df4e63d6243a2
ChristopherLMiller <chris@christopherleemiller.me>
2020-04-06 07:49:44 -0400

    don't exclude parenthesis

--- utils/functions/truncate.tsx
+++ utils/functions/truncate.tsx
@@ -1,4 +1,4 @@
 export const truncate = (text: string, count: number): string | undefined => {
-    const matches = text.match(/([a-zA-Z ,'-]*[.?!\n])/gm)
+    const matches = text.match(/([a-zA-Z ,'-)(]*[.?!\n])/gm)
     return matches?.slice(0, count).join('  ');
 }
