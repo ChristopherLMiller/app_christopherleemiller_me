78278c30a0bc0c6f5a1134a40a5017b20a2f5ca0
ChristopherLMiller <chris@christopherleemiller.me>
2020-04-15 08:53:58 -0400

    modify image component to accept all cloudinary options

--- components/elements/Image.tsx
+++ components/elements/Image.tsx
@@ -1,43 +1,13 @@
-import { unstable_createResource } from 'react-cache';
-import { Suspense } from 'react';
-
-const ImageResource = unstable_createResource(
-  (source: string) =>
-    new Promise(resolve => {
-      const img = new Image();
-      img.src = source;
-      img.onload = resolve;
-    })
-);
+import { FunctionComponent } from "react"
+import { ImageURL, iImageOptions } from "../../utils/functions/imageURL"
 
 interface iImage {
-  src: string;
-  alt: string;
+  file?: string;
+  options?: iImageOptions;
 }
 
-export const Img = ({ src, alt, ...props }: iImage) => {
-  ImageResource.read(src);
-  return <img src={src} alt={alt} {...props} />
-}
+const Image:FunctionComponent<iImage> = ({file, options}) => (
+  <img src={ImageURL(file, options)} />
+)
 
-
-
-const ImageComponent = ({ image, alt, render }: any) => (
-  render ?
-    <div>
-      <Suspense fallback={
-        <div>
-          <img className="blurry" src={image.small} alt={alt} />
-          <span>Loading...</span>
-        </div>
-      }>
-        <div>
-          <img src={image.large} alt={alt} />
-        </div>
-      </Suspense>
-    </div>
-    :
-    <div />
-);
-
-export { ImageComponent as Image }
+export { Image }
