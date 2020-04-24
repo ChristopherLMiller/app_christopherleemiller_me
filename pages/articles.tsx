68fd8c9cf5a855832016a3b999c40817ae95f353
ChristopherLMiller <chris@christopherleemiller.me>
2020-04-24 01:45:58 -0400

    alert query

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,15 +1,15 @@
-import ReactMarkdown from 'react-markdown';
-import { FunctionComponent } from 'react';
-import { useQuery } from 'react-apollo';
-import { useRouter } from 'next/router';
-import { BriefArticle } from '../components/articles/Brief';
-import Card from '../components/Card';
-import { ARTICLES_QUERY } from '../utils/query';
-import { iData } from '../components/articles/Types';
-import { Loader } from '../components/elements/Loader';
-import { PER_PAGE } from '../config';
-import { truncate } from '../utils/functions/truncate';
-import { Layout } from '../components/layout/PageLayout';
+import ReactMarkdown from "react-markdown";
+import { FunctionComponent } from "react";
+import { useQuery } from "react-apollo";
+import { useRouter } from "next/router";
+import { BriefArticle } from "../components/articles/Brief";
+import Card from "../components/Card";
+import { ARTICLES_QUERY } from "../utils/query";
+import { iData } from "../components/articles/Types";
+import { Loader } from "../components/elements/Loader";
+import { PER_PAGE } from "../config";
+import { truncate } from "../utils/functions/truncate";
+import { Layout } from "../components/layout/PageLayout";
 
 const title = `From My Desk`;
 const description = `Archives concerning all matters web development and beyond`;
@@ -45,8 +45,9 @@ const ArticlesPage: FunctionComponent = () => {
           <hr />
           <p>
             Sorry. Something happened and we can't seem to load data right now.
-            Possibly you're offline and if not please let us know.  I'm sure the
-            above text doesn't make any sense but it will help me figure out the problem.
+            Possibly you're offline and if not please let us know. I'm sure the
+            above text doesn't make any sense but it will help me figure out the
+            problem.
           </p>
         </Card>
       </Layout>
@@ -57,7 +58,7 @@ const ArticlesPage: FunctionComponent = () => {
     <Layout meta={{ title, description, useSEO: true, path: `/articles` }}>
       <Loader isLoading={loading} />
       {data !== undefined &&
-        data.articles.map(article => (
+        data.articles.map((article) => (
           <BriefArticle article={article} key={article.id}>
             <ReactMarkdown source={truncate(article.content, 3)} />
           </BriefArticle>
