937c6d1e1a31a92e7e3e67f1dfe1f377b8fac1c0
ChristopherLMiller <chris@christopherleemiller.me>
2020-04-30 10:23:48 -0400

    add typings back to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,54 +1,15 @@
-/*import Document, {
-  Head,
-  Main,
-  NextScript,
-  DocumentProps,
-  DocumentContext,
-} from 'next/document';
-
-import { ServerStyleSheet } from 'styled-components';
-
-interface IDocument {
-  styleTags: string;
-}
-export default class MyDocument extends Document<DocumentProps & IDocument> {
-  static async getInitialProps({ renderPage }: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    try {
-      const page = renderPage(App => props =>
-        sheet.collectStyles(<App {...props} />)
-      );
-      const styleTags = sheet.getStyleElement();
-      return { ...page, styleTags };
-    } finally {
-      sheet.seal();
-    }
-  }
-
-  render() {
-    return (
-      <html lang="en">
-        <Head>{this.props.styleTags}</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
-}*/
-import Document from "next/document";
+import Document, { DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
+          enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
 
