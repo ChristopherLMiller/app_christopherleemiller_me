566ea14483b9b8fe3d09623414f480b3f9b7b506
ChristopherLMiller <chris@christopherleemiller.me>
2020-07-03 07:11:23 -0400

    convert nav to use json data

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,24 +1,7 @@
 import styled from "styled-components";
 import { NavItem } from "components/layout/elements/NavItem";
 import { motion } from "framer-motion";
-//@ts-ignore
-import IosHomeOutline from "react-ionicons/lib/IosHomeOutline";
-//@ts-ignore
-import IosMailOutline from "react-ionicons/lib/IosMailOutline";
-//@ts-ignore
-import IosQuoteOutline from "react-ionicons/lib/IosQuoteOutline";
-//@ts-ignore
-import IosPaperPlaneOutline from "react-ionicons/lib/IosPaperPlaneOutline";
-//@ts-ignore
-import IosPersonOutline from "react-ionicons/lib/IosPersonOutline";
-//@ts-ignore
-import IosGitBranch from "react-ionicons/lib/IosGitBranch";
-//@ts-ignore
-import IosPhotosOutline from "react-ionicons/lib/IosPhotosOutline";
-//@ts-ignore
-import IosCodeWorking from "react-ionicons/lib/IosCodeWorking";
-//@ts-ignore
-import IosCogOutline from "react-ionicons/lib/IosCogOutline";
+import { useState, useEffect } from "react";
 
 /*
 TODO: do this but with motion
@@ -37,46 +20,31 @@ const NavStyles = styled(motion.ul)`
 `;
 
 const Nav = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [navLinks, setNavLinks] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await import("data/nav.json");
+
+      setLoading(false);
+      //@ts-ignore
+      setNavLinks(data.items);
+    }
+    fetchData();
+  });
   return (
     <NavStyles initial="exit" animate="enter">
-      <NavItem title="Home" isActivePaths={["/"]} href="/">
-        <IosHomeOutline color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem isActivePaths={["/blog", "/post"]} href="/blog" title="My Blog">
-        <IosQuoteOutline color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem isActivePaths={["/about"]} href="/about" title="About Me">
-        <IosPersonOutline color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem isActivePaths={["/uses"]} href="/uses" title="Uses">
-        <IosGitBranch color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem isActivePaths={["/projects"]} href="/projects" title="Projects">
-        <IosCodeWorking color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem isActivePaths={["/models"]} href="/models" title="Models">
-        <IosPaperPlaneOutline color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem
-        isActivePaths={["/galleries"]}
-        href="/galleries"
-        title="Galleries"
-      >
-        <IosPhotosOutline color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem
-        isActivePaths={["/contact-me"]}
-        href="/contact-me"
-        title="Contact Me"
-      >
-        <IosMailOutline color="#FFF" fontSize="50px" />
-      </NavItem>
-      <NavItem
-        href="https://strapi.christopherleemiller.me/dashboard"
-        title="Strapi Backend"
-      >
-        <IosCogOutline color="#FFF" fontSize="50px" />
-      </NavItem>
+      {!isLoading &&
+        navLinks != undefined &&
+        navLinks.map((item: any) => (
+          <NavItem
+            title={item.title}
+            isActivePaths={item.activePaths}
+            href={item.href}
+            key={item.href}
+          ></NavItem>
+        ))}
     </NavStyles>
   );
 };
